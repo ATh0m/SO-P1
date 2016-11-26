fdbf5fc8d16333af92ec63e6d76b54b3170376a5
wiatrak2 <wpratkowiecki@gmail.com>
2016-11-26 21:28:27 +0100

    added buffer template class

---
+++ buffer.h
@@ -0,0 +1,63 @@
+#ifndef buffer_h
+#define buffer_h
+#include <mutex>
+
+template<typename T>
+class Buffer
+{
+public:
+    T* buffer;
+    const unsigned int capacity;
+    int front;
+    int last;
+    int amount;
+    
+    std::mutex m;
+    std::condition_variable is_full;
+    std::condition_variable is_empty;
+    
+    Buffer ( const unsigned int capacity )
+    : capacity { capacity },
+    front { 0 },
+    last { 0 },
+    amount { 0 }
+    {
+        buffer = new T [capacity];
+    }
+    
+    ~Buffer()
+    {
+        delete[] buffer;
+    }
+    
+    void produce( T product )
+    {
+        std::unique_lock < std::mutex > lock (m);
+        is_full.wait(lock, [this] () { return amount != capacity; });
+        
+        buffer[last] = product;
+        ++ last %= capacity;
+        ++ amount;
+        
+        is_empty.notify_one();
+        lock.unlock();
+    }
+    
+    T consume()
+    {
+        std::unique_lock< std::mutex > lock (m);
+        is_empty.wait(lock, [this] () { return amount != capacity; });
+        
+        T result = buffer [front];
+        ++ front %= capacity;
+        -- amount;
+        
+        is_full.notify_one();
+        lock.unlock();
+        
+        return result;
+    }
+    
+};
+
+#endif /* buffer_h */
