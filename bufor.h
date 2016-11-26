172858e357f8c6f8a9282660e9b7e6908e0fabcc
wiatrak2 <wpratkowiecki@gmail.com>
2016-11-26 18:29:38 +0100

    create .cpp files

--- bufor.h
+++ bufor.h
@@ -1,13 +1,10 @@
-//
-//  bufor.h
-//  P1
-//
-//  Created by Wojciech Pratkowiecki on 26.11.2016.
-//  Copyright © 2016 Wojciech Pratkowiecki. All rights reserved.
-//
-
 #ifndef bufor_h
 #define bufor_h
-
+#include <iostream>
+template<typename T>
+class bufor
+{
+    
+};
 
 #endif /* bufor_h */
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "bufor.h"
+int main() {
+
+    return 0;
+}
