522939a7ee77862e20d1b4a9d22fb92c9fd955dd
wiatrak2 <wpratkowiecki@gmail.com>
2016-11-26 23:29:29 +0100

    add assignment constructor for ingredient

--- ingredient.h
+++ ingredient.h
@@ -1,17 +1,31 @@
 #ifndef ingredient_h
 #define ingredient_h
-
+#include <utility>
 #include <string>
-
+#include <iostream>
 class Ingredient {
 public:
-    const std::string name;
-    const unsigned int price;
+    std::string name;
+    unsigned int price;
 
     Ingredient (const std::string & name, const unsigned int price)
     :   name {name}, 
         price {price}
     {}
+    
+    Ingredient (const Ingredient& i)
+    : name {i.name},
+    price {i.price}
+    {}
+    
+    Ingredient& operator = (const Ingredient& i)
+    {
+        if(&i == this)
+            return *this;
+        name = i.name;
+        price = i.price;
+        return *this;
+    }
 };
 
 #endif /* ingredient_h */
