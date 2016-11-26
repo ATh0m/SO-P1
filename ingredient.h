636d8b47f8ed75a03441c3f4b6c89423b8c368da
Tomasz Nanowski <tomasz.nanowski@gmail.com>
2016-11-26 19:26:47 +0100

    Added Ingredient class definition

--- ingredient.h
+++ ingredient.h
@@ -1,5 +1,16 @@
 #ifndef ingredient_h
 #define ingredient_h
 
+#include <string>
+
+class Ingredient {
+    const std::string name;
+    const unsigned int price;
+
+    Ingredient (const std::string & name, const unsigned int price)
+    :   name {name}, 
+        price {price}
+    {}
+};
 
 #endif /* ingredient_h */
